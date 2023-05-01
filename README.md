70a863e7b1f4b0180fb170de120ff6932c152e00
irj1001 <74235665+irj1001@users.noreply.github.com>
2023-05-01 21:40:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 08MBID---Visualizacion-de-datos
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-tsg3ly)
---
+++ index.js
@@ -0,0 +1,20 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+
+
+
+<div id="app">
+  <span class="negrita">Nombre:</span> Irene Ruiz Juez
+  <br />
+  <span class="negrita">URL:</span>
+    <a href="https://js-tsg3ly.stackblitz.io" target="_blank">
+    https://js-tsg3ly.stackblitz.io
+    </a>
+    <br />
+    <span class="negrita">GitHub:</span>
+
+  <h1>Visualización AG1 </h1>
+ </div> 
