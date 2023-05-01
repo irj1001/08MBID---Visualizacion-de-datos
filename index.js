2ca36d27038c3ca1bbccaf4f55d019a2adff4f62
irj1001 <74235665+irj1001@users.noreply.github.com>
2023-05-01 21:41:32 +0200

    1 file modified

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 
-
-
 <div id="app">
   <span class="negrita">Nombre:</span> Irene Ruiz Juez
   <br />
   <span class="negrita">URL:</span>
-    <a href="https://js-tsg3ly.stackblitz.io" target="_blank">
+  <a href="https://js-tsg3ly.stackblitz.io" target="_blank">
     https://js-tsg3ly.stackblitz.io
-    </a>
-    <br />
-    <span class="negrita">GitHub:</span>
-
+  </a>
+  <br />
+  <span class="negrita">GitHub:</span>
+  <a
+    href="https://github.com/irj1001/08MBID---Visualizacion-de-datos"
+    target="_blank"
+  >
+    https://github.com/irj1001/08MBID---Visualizacion-de-datos
+  </a>
   <h1>Visualización AG1 </h1>
- </div> 
+</div>;
